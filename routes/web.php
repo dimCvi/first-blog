65124c467e61443f2aa25b8ce52fb07f1d435393
dimCvi <dimitrijecvijic0@gmail.com>
2020-04-25 15:05:16 +0200

    feat: auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth')->namespace('Admin')->group(function() {
+    $d = 'IndexController@';
+    Route::get('admin', $d . 'index')->name('admin.index.index');
+});
