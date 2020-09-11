298f81c0bb377a895b332c836aef211f0bd653ed
ACvijic <aleksa.cvijic@cubes.rs>
2020-09-11 12:17:27 +0200

    feat(post): seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,5 +10,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(PostCategoriesSeeder::class);
     }
 }
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        DB::table('posts')->truncate();
+
+        for ($i=1; $i <= 100; $i++) {
+            DB::table('posts')->insert([
+                'title' => $faker->city,
+                'text' => $faker->realText(),
+                'user_id' => User::first()->id,
+                'featured' => $faker->boolean(),
+                'status' => $faker->boolean(),
+                'views' => $faker->randomFloat(0, 100, 10000),
+                'comments' => $faker->randomFloat(0, 100, 10000),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
