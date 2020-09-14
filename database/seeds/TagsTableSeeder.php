f0ef86ca5b2562baa3aa7ad8ba843a10ff26eb17
ACvijic <aleksa.cvijic@cubes.rs>
2020-09-14 15:06:57 +0200

    feat(tags): seeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        DB::table('tags')->truncate();
+
+        for ($i=1; $i <= 15; $i++) {
+            DB::table('tags')->insert([
+                'name' => $faker->city,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
