dcdbe6ae412afd3d1083e0dc82b828c197c07378
ACvijic <aleksa.cvijic@cubes.rs>
2020-12-27 14:56:10 +0100

    feat(tags): post tag seeder

---
+++ database/seeds/PostTagsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTagsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('post_tags')->truncate();
+
+        $postIds = DB::table('posts')->get()->pluck('id');
+        
+        $tagIds = DB::table('tags')->get()->pluck('id');
+        
+        foreach ($postIds as $postId) {
+            $randomCateogyIds = $tagIds->random(5);
+            foreach ($randomCateogyIds as $tagId) {
+                DB::table('post_tags')->insert([
+                    'post_id' => $postId,
+                    'tag_id' => $tagId,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]);
+            }
+        }
+    }
+}
