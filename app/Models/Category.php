8aeae652e4f2a05403a161e160a0f25050f05743
ACvijic <aleksa.cvijic@cubes.rs>
2020-12-27 15:03:16 +0100

    update(categories): added function posts

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,12 +1,21 @@
 <?php
 
 namespace App\Models;
+use App\Models\Post;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $table = 'categories';
+
+    public function posts() {
+        return $this->hasMany(
+            Post::class,
+            'post_categories',
+            'id'
+        );
+    }
 
     protected $fillable = [
         'header',
