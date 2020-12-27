df848205b5dda1480ba76571f76995efe4b214e9
ACvijic <aleksa.cvijic@cubes.rs>
2020-12-27 15:08:00 +0100

    update(post): added functions in model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -20,8 +21,29 @@ class Post extends Model
         'photo',
     ];
 
-    public function author() {
+    public function author() 
+    {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function categories() 
+    {
+        return $this->belongsToMany(
+            Category::class,
+            'post_categories',
+            'post_id',
+            'category_id'
+        );
+    }
+
+    public function tags() 
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags',
+            'post_id',
+            'tag_id'
+        );
     }
     
     public function deletePhoto()
